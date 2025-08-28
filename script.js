7822e7b7d4b2a729d0b0035f1a39e56844684512
RiccardoOttavi <ottavi.ps@libero.it>
2025-08-28 17:10:54 +0200

    sviluppa la logica

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const buttons = document.querySelectorAll("#keyboard button");
 const display = document.getElementById("display");
-
+let displayContent = [];
 
 buttons.forEach(btn => {
   btn.addEventListener("click", () => {
-    display.textContent = btn.textContent; // scrive il numero cliccato
+    displayContent.push(btn.textContent); // s
+    display.textContent = displayContent.join("") // scrive il numero cliccato
   });
 });
